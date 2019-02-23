bdfc1f79365e03f42d299f9165b995982d989f58
Michael-Salato-DU <42783058+Michael-Salato-DU@users.noreply.github.com>
2019-02-22 21:16:58 -0500

    not finished

--- Project2/src/app/app.component.ts
+++ Project2/src/app/app.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor() {}
+
+
+
+  ngOnInit() {
+    
+  }
   title = 'BADS Hotel';
 
   n =  new Date();
-   y = this.n.getFullYear();
-   m = this.n.getMonth() + 1;
-   d = this.n.getDate();
+  y = this.n.getFullYear();
+  m = this.n.getMonth() + 1;
+  d :number = this.n.getDate();
 
-   today :string = this.y + "-0" + this.m + "-" + this.d
+  mformatted :string = ("0" + this.m).slice(-2);
+  dformatted :string = ("0" + this.d).slice(-2);
+
+  plzWork(d);
+  plzWork(m);
+  plzWork(num :number) {
+    console.log("hi")
+    // if (num < 10) {
+    //   console.log("less than 10")
+    // }
+    // console.log("not less than 10")
+  }
+
+   today :string = this.y + "-" + this.mformatted + "-" + this.dformatted
   
    startDate :string = this.today;
    endDate :string; // preferably initialze to tommorow's date
