c55c815037f2f3cc2ec836b91efe8110ebe92c73
Bas3ad <pas1@umbc.edu>
2019-02-23 15:19:52 -0500

    Working dates inputs now

--- Project2/src/app/app.component.ts
+++ Project2/src/app/app.component.ts
@@ -14,39 +14,42 @@ export class AppComponent implements OnInit{
   ngOnInit() {
     
   }
+ 
   title = 'BADS Hotel';
 
-  n =  new Date();
+  // getting today's date
+  n = new Date();
   y = this.n.getFullYear();
   m = this.n.getMonth() + 1;
-  d :number = this.n.getDate();
+  d = this.n.getDate();
 
+  // reformating the month and day to be two digit 
   mformatted :string = ("0" + this.m).slice(-2);
   dformatted :string = ("0" + this.d).slice(-2);
 
-  plzWork(d);
-  plzWork(m);
-  plzWork(num :number) {
-    console.log("hi")
-    // if (num < 10) {
-    //   console.log("less than 10")
-    // }
-    // console.log("not less than 10")
-  }
+  // today is always today's date
+  today :string = this.y + "-" + this.mformatted + "-" + this.dformatted;
+  // startDate will start as today, but will change based on the input
+  startDate :string = this.today;
+  // preferably initialze to tommorow's date when startDate get initialzed
+  endDate :string; 
 
-   today :string = this.y + "-" + this.mformatted + "-" + this.dformatted
-  
-   startDate :string = this.today;
-   endDate :string; // preferably initialze to tommorow's date
 
    // when you click the submit button,
    // it will set endDate to startDate if endDate is undefined
+   
+ 
    submitDates() {
-     if (this.endDate == "undefined") {
-       this.endDate = this.startDate;
+     if (this.endDate == null) {
+       this.endDate = this.startDate.substring(0,8)+(+this.startDate.substring(8,10)+1);
      }
-    
-     console.log(this.endDate);
+     /*  
+
+      Here you can get the Arrival Date as (startDate) which is a string formated yyyy-mm-dd
+
+      Also you back Departure Date as (endDate) which is a string formated yyyy-mm-dd
+
+     */
    }
 
 }
