967bd7a5af3d4724b9f242d55a5e60631b2788f3
Bas3ad <pas1@umbc.edu>
2019-02-23 16:43:45 -0500

    BADS update

--- Project2/src/app/app-routing.module.ts
+++ Project2/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DateSelectionComponent} from 'src/app/components/date-selection/date-selection.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'date-selection',component : DateSelectionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Project2/src/app/app.component.ts
+++ Project2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
 
-  constructor() {}
+  constructor(private loginserivce: LoginService) {}
 
 
 
@@ -17,41 +18,10 @@ export class AppComponent implements OnInit{
  
   title = 'BADS Hotel';
 
-  // getting today's date
-  n = new Date();
-  y = this.n.getFullYear();
-  m = this.n.getMonth() + 1;
-  d = this.n.getDate();
 
-  // reformating the month and day to be two digit 
-  mformatted :string = ("0" + this.m).slice(-2);
-  dformatted :string = ("0" + this.d).slice(-2);
-
-  // today is always today's date
-  today :string = this.y + "-" + this.mformatted + "-" + this.dformatted;
-  // startDate will start as today, but will change based on the input
-  startDate :string = this.today;
-  // preferably initialze to tommorow's date when startDate get initialzed
-  endDate :string; 
-
-
-   // when you click the submit button,
-   // it will set endDate to startDate if endDate is undefined
-   
- 
-   submitDates() {
-     if (this.endDate == null) {
-       this.endDate = this.startDate.substring(0,8)+(+this.startDate.substring(8,10)+1);
-     }
-     /*  
-
-      Here you can get the Arrival Date as (startDate) which is a string formated yyyy-mm-dd
-
-      Also you back Departure Date as (endDate) which is a string formated yyyy-mm-dd
-
-     */
-   }
-
+   login(){
+    this.loginserivce.login();
+  }
 }
 
 
