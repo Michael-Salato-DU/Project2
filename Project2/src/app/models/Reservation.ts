57db61a5c9813e1a4034e5a6a8059a9aaf16c089
Michael Salato <42783058+Michael-Salato-DU@users.noreply.github.com>
2019-02-24 13:23:16 -0500

    Update Reservation.ts

--- Project2/src/app/models/Reservation.ts
+++ Project2/src/app/models/Reservation.ts
@@ -1,9 +1,9 @@
 export class Reservation{
     reservation_id :number;
     customer_id :number;
-    start_date :number;
-    end_date :number;
-    constructor(reservation_id :number, customer_id :number, start_date :number, end_date :number){
+    start_date :string;
+    end_date :string;
+    constructor(reservation_id :number, customer_id :number, start_date :string, end_date :string){
     this.reservation_id = reservation_id;
     this.customer_id = customer_id;
     this.start_date = start_date; 
