46cb6e7eae9812599d5a11ef459a99b59c55472e
Michael Salato <42783058+Michael-Salato-DU@users.noreply.github.com>
2019-02-24 13:25:41 -0500

    Delete Room.ts

--- Project2/src/app/models/Room.ts
+++
@@ -1,8 +0,0 @@
-export class Room{
-    room_id :number;
-    number_of_beds :number;
-    constructor(room_id :number, number_of_beds :number){
-        this.room_id = room_id;
-        this.number_of_beds =number_of_beds;
-    }
-}
