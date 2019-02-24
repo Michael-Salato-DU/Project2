a25af40bd158a979451ed4a1600a48412ebbf108
Brandon Bostic <bbostic2@mix.wvu.edu>
2019-02-23 23:00:09 -0500

    added html and css for register page, added bootstrap

--- Project2/src/app/app-routing.module.ts
+++ Project2/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from 'src/app/components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'register', component : RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Project2/src/app/app.module.ts
+++ Project2/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
