37ad6c07d2d4365eb92ffad4d331954b96671293
Brandon Bostic <bbostic2@mix.wvu.edu>
2019-02-24 14:13:16 -0500

    fixed somehow

---
+++ Project2/src/app/models/Room.ts
@@ -0,0 +1,8 @@
+export class Room{
+    room_id :number;
+    number_of_beds :number;
+    constructor(room_id :number, number_of_beds :number){
+        this.room_id = room_id;
+        this.number_of_beds = number_of_beds;
+    }
+}
