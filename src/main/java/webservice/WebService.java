ddbda115e9bac4deef35b7453089d4c82a596fa2
Bas3ad <pas1@umbc.edu>
2019-02-25 14:33:06 -0500

    Peter learns git

--- src/main/java/webservice/WebService.java
+++ src/main/java/webservice/WebService.java
@@ -21,12 +21,13 @@ public class WebService {
 	
 	public static void register (HttpServletRequest request, HttpServletResponse response)
 	{
-		String first_name = request.getParameter("first");
-		String last_name = request.getParameter("last");
-		String username = request.getParameter("uname");
-		String password = request.getParameter("pass");
+		System.out.println("Here");
+		String first_name = request.getParameter("first_name");
+		String last_name = request.getParameter("last_name");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 		String email = request.getParameter("email");
-		String phone_number = request.getParameter("phone#");
+		String phone_number = request.getParameter("phone");
 		String address = request.getParameter("address");
 		String hashedPassword = CustomerService.HashPassword(password);
 		Customer c = new Customer(0, first_name, last_name, username, hashedPassword, email, phone_number, address);
@@ -55,7 +56,7 @@ public class WebService {
 		{
 			String room_id = room.getString("room_id");
 			String number_of_beds = room.getString("number_of_beds");
-			Room roomObj = new Room(Integer.parseInt(room_id), Byte.parseByte(number_of_beds);
+			Room roomObj = new Room(Integer.parseInt(room_id), Byte.parseByte(number_of_beds));
 			roomList.add(roomObj);
 		}
 		ObjectMapper om = new ObjectMapper();
@@ -113,7 +114,7 @@ public class WebService {
 		String end_date = request.getParameter("end_Date");
 		RoomDAO rd = new RoomDAO();
 		ArrayList<Room> al = new ArrayList<Room>();
-		al = rd.getAvailableRooms();
+//		al = rd.getAvailableRooms();
 		ObjectMapper om = new ObjectMapper();
 		try
 		{
