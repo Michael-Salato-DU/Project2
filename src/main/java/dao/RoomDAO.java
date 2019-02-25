494f75a1240245998015db0e6900cb795dbbc27a
Michael-Salato-DU <42783058+Michael-Salato-DU@users.noreply.github.com>
2019-02-25 14:18:28 -0500

    getAllAvailableRooms update
    getAllAvailableRooms method should be working properly now. I tested it in RoomDAOTesting.java and it works. However, I had to write an insert statement for the reservation_room table in SQL developer because I don't know how to create a reservation_room record in Hibernate.

--- src/main/java/dao/RoomDAO.java
+++ src/main/java/dao/RoomDAO.java
@@ -20,15 +20,21 @@ public class RoomDAO implements IRoom{
 	public ArrayList<Room> getAvailableRooms(String start_date, String end_date) {
 		Session session = sf.openSession();
 		
-		String sql = "SELECT room.room_id, number_of_beds " + 
-				"FROM " + 
-				"room " + 
-				"INNER JOIN reservation_room " + 
-				"ON room.room_id = reservation_room.room_id " + 
-				"INNER JOIN reservation " + 
-				"ON resevation.reservation_id = reservation_room.reservation_id " + 
-				"WHERE (? NOT BETWEEN start_date AND end_date) " + 
-				"AND (? NOT BETWEEN start_date AND end_date)";
+		String sql = "SELECT room_id, number_of_beds\r\n" + 
+					"FROM room\r\n" + 
+					"WHERE room_id NOT IN (\r\n" + 
+					"    SELECT UNIQUE room.room_id\r\n" + 
+					"    FROM room\r\n" + 
+					"    INNER JOIN reservation_room\r\n" + 
+					"    ON room.room_id = reservation_room.rooms_room_id\r\n" + 
+					"    INNER JOIN reservation\r\n" + 
+					"    ON reservation.reservation_id = reservation_room.reservations_reservation_id\r\n" + 
+					"    WHERE (? BETWEEN start_date AND end_date)\r\n" + 
+					"    OR (? BETWEEN start_date AND end_date)\r\n" + 
+					"    OR (start_Date BETWEEN ? AND ?)\r\n" + 
+					"    OR (end_date BETWEEN ? AND ?)\r\n" + 
+					")\r\n" + 
+					"ORDER BY room_id";
 		
 		SQLQuery sq = session.createSQLQuery(sql);
 		
@@ -39,8 +45,13 @@ public class RoomDAO implements IRoom{
 		// set start and end parameters
 		sq.setParameter(0, start);
 		sq.setParameter(1, end);
+		sq.setParameter(2, start);
+		sq.setParameter(3, end);
+		sq.setParameter(4, start);
+		sq.setParameter(5, end);
 	
 		List rooms = sq.list();
+		
 		
 		session.close();
 		return new ArrayList<Room>(rooms);
--- src/test/java/daotests/RoomDAOTesting.java
+++ src/test/java/daotests/RoomDAOTesting.java
@@ -1,23 +1,42 @@
 package daotests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import dao.ReservationDAO;
 import dao.RoomDAO;
+import model.Reservation;
+import model.Room;
 
 public class RoomDAOTesting {
-  
-	static RoomDAO rd = new RoomDAO();
+	
+	static ReservationDAO rd = new ReservationDAO();
+	static RoomDAO rmd = new RoomDAO();
+	
+//  @BeforeSuite
+//  public void setup() {
+//	  Reservation reservation = new Reservation(-2, -2, java.sql.Date.valueOf("2011-11-15"), java.sql.Date.valueOf("2011-11-25"));
+//	  List<Room> reservedrooms = new ArrayList<Room>();
+//	  reservedrooms.add(new Room(0, (byte) 1));
+//	  reservedrooms.add(new Room(2, (byte) 1));
+////	  reservation.setRooms(reservedrooms);
+//	  
+//	  rd.createReservation(reservation);
+//  }
 	
 	@Test
-  public void testGetAllRooms()
+  public void testGetAll21Rooms()
   {
-	  Assert.assertTrue(rd.getAvailableRooms("2018-11-15", "2018-11-15").size() >= 1);
+	  Assert.assertTrue(rmd.getAvailableRooms("2010-11-15", "2010-11-25").size() == 21);
   }
 	
   @Test
-  public void testGetAllRoomsFalse()
+  public void testGet19Rooms()
   {
-	  Assert.assertFalse(rd.getAvailableRooms("2017-11-15", "2017-11-25").size() >= 1);
+	  Assert.assertTrue(rmd.getAvailableRooms("2011-11-15", "2011-11-25").size() == 19);
   }
 }
