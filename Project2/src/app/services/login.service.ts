64fd567c0b6e113317896fc7b87ef6b9b137f9c2
Brandon Bostic <bbostic2@mix.wvu.edu>
2019-02-26 11:47:39 -0500

    login service made

--- Project2/src/app/services/login.service.ts
+++ Project2/src/app/services/login.service.ts
@@ -1,16 +1,28 @@
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+import { Customer } from 'src/app/models/Customer';
 import { Injectable } from '@angular/core';
-// import { HttpClient } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  registerUrl :string  = "http://ec2-52-15-141-97.us-east-2.compute.amazonaws.com:8080/Project2/login.do";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/x-www-form-urlencoded',
+      'X-Requested-With': 'XMLHttpRequest'
+    })
+  };
+  constructor(private http :HttpClient) { }
 
-  // within constructor ( private http:HttpClient )
-  constructor() { }
 
-  
-  sendLogin(uname:string, psw: string){
+  sendLogin(uname:string, psw: string):Observable<Customer>{
+    // this.lCustomer.first_name = uname;
+    // console.log("psw: " + this.lCustomer.password);
+    // console.log("name: " +this.lCustomer.first_name);
 
+    return this.http.get<Customer>(this.registerUrl + "?username="+uname + "&password="+psw, this.httpOptions);
   }
 }
