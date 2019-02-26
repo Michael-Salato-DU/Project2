37fdd78ffcd54319d2f3c145f65f9c68072fe8b1
Bostic <bbostic2@mix.wvu.edu>
2019-02-25 19:17:43 -0500

    Used 2 way property binding for data input

--- Project2/src/app/components/register/register.component.ts
+++ Project2/src/app/components/register/register.component.ts
@@ -16,12 +16,13 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
 
   }
-  
+
   newCustomer :Customer;
   first_name :string;
   last_name :string;
   username :string;
   password :string;
+  rpsw :string;
   email :string;
   phone_number :string;
   address :string;
@@ -29,15 +30,8 @@ export class RegisterComponent implements OnInit {
   registerResponse :Observable<string>;
 
   register(){
-    // console.log((<HTMLSelectElement>document.getElementById('first_name')).value);
-    this.first_name = (<HTMLSelectElement>document.getElementById('first_name')).value;
-    this.last_name  = (<HTMLSelectElement>document.getElementById('last_name')).value;
-    this.username = (<HTMLSelectElement>document.getElementById('username')).value;
-    this.password = (<HTMLSelectElement>document.getElementById('rpsw')).value;
-    this.email = (<HTMLSelectElement>document.getElementById('email')).value;
-    this.phone_number = (<HTMLSelectElement>document.getElementById('phone_number')).value;
-    this.address = (<HTMLSelectElement>document.getElementById('address')).value;
     this.newCustomer = new Customer(0,this.first_name, this.last_name, this.username, this.password,this.email, this.phone_number, this.address);
+    console.log(this.newCustomer);
     this.rs.sendRegister(this.newCustomer).subscribe((response)=>{console.log("success")},(response)=>{console.log("failure")});
   }
 
