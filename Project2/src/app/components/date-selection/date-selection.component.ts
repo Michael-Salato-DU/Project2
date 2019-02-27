97baea37586eb44da92738836f65aba6338c6353
Brandon Bostic <bbostic2@mix.wvu.edu>
2019-02-27 10:39:56 -0500

    date selection should work now

--- Project2/src/app/components/date-selection/date-selection.component.ts
+++ Project2/src/app/components/date-selection/date-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReservationService } from 'src/app/services/reservation.service'
 
 @Component({
   selector: 'app-date-selection',
@@ -7,15 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DateSelectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private rs :ReservationService) { }
 
   ngOnInit() {
   }
   today :Date = new Date();
   tomorrow :Date = new Date(this.today.getTime() + (1000 * 60 * 60 * 24));
-  startDate :Date;
+  startDate :Date = new Date();
   endDate :Date;
   submitDates() {
-    
+    if( this.endDate == null){
+      alert("enddate not picked");
+    }
+    else if( this.endDate < this.startDate){
+      alert("endDate greater than StartDate");
+    }
+    else if ( this.endDate == this.startDate){
+      alert("you must depart on at least the next day")
+    }
+    else if ( this.startDate < this.endDate){
+      this.rs.getReservations(this.startDate, this.endDate);
+    }
   }
 }
--- Project2/src/app/services/reservation.service.ts
+++ Project2/src/app/services/reservation.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Room } from '../models/Room';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  constructor(private http :HttpClient) { }
+  getAvailableRoomsUrl :string  = "http://ec2-52-15-141-97.us-east-2.compute.amazonaws.com:8080/Project2/getAvailableRooms.do";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/x-www-form-urlencoded',
+      'X-Requested-With': 'XMLHttpRequest'
+    })
+  };
+  
+  getReservations(start :Date, end :Date):Observable<Array<Room>>{
+    console.log(start +"\n" + end);
+    return this.http.post<Array<Room>>(this.getAvailableRoomsUrl, "?start="+start + "end="+end, this.httpOptions);
+  }
 }
