2fa23533cffe5ba4693c860c0b40c42282705b00
Bas3ad <pas1@umbc.edu>
2019-02-26 19:44:21 -0500

    changes to Register Model

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -10,58 +10,55 @@ import dao.CustomerDAO;
 import model.Customer;
 
 public class CustomerService {
-	
+
 	private static CustomerDAO cd = new CustomerDAO();
-	
+
 	public static boolean addUser(Customer c) {
 		return cd.addUser(c);
 	}
-	
-	public static boolean Login (String username, String password) {
+
+	public static boolean Login(String username, String password) {
 		return cd.Login(username, password);
 	}
-	
+
 	public static boolean UpdateUser(Customer c) {
 		return cd.UpdateUser(c);
 	}
-	
+
 	public static Customer getCustomer(String username) {
 		return cd.getCustomer(username);
 	}
-	
+
 	/**
 	 * needs to be finished
+	 * 
 	 * @param password
 	 * @return
 	 */
-	//Code 'borrowed' from Howtodoinjava.com//
-	
-	public static String hashPassword(String password)
-	{
+	// Code 'borrowed' from Howtodoinjava.com//
+
+	public static String hashPassword(String password) {
 		String hashedPassword = null;
-		
+
 		try {
 			// Create MessageDigest instance for MD5
 			MessageDigest md = MessageDigest.getInstance("MD5");
-			//Add password bytes to digest
+			// Add password bytes to digest
 			md.update(password.getBytes());
-			//Get the hash's bytes
+			// Get the hash's bytes
 			byte[] bytes = md.digest();
-			//This bytes[] has bytes in decimal format;
-			//Convert it to hexadecimal format
+			// This bytes[] has bytes in decimal format;
+			// Convert it to hexadecimal format
 			StringBuilder sb = new StringBuilder();
-			for(int i=0; i< bytes.length ;i++)
-			{
+			for (int i = 0; i < bytes.length; i++) {
 				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
 			}
-			//Get complete hashed password in hex format
+			// Get complete hashed password in hex format
 			hashedPassword = sb.toString();
 			return hashedPassword;
-    }
-    catch (NoSuchAlgorithmException e)
-    {
-        e.printStackTrace();
-        return hashedPassword;
-    }
-}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return hashedPassword;
+		}
+	}
 }
