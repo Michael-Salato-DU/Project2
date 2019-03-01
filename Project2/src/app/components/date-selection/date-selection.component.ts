90792b8afa937ea488e79acea6b6682971e793d7
Michael-Salato-DU <42783058+Michael-Salato-DU@users.noreply.github.com>
2019-03-01 13:10:02 -0500

    Can view and select rooms to reserve

--- Project2/src/app/components/date-selection/date-selection.component.ts
+++ Project2/src/app/components/date-selection/date-selection.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReservationService } from 'src/app/services/reservation.service'
 import { Observable } from 'rxjs';
 import {Room} from 'src/app/models/Room';
+import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-date-selection',
@@ -10,7 +11,8 @@ import {Room} from 'src/app/models/Room';
 })
 export class DateSelectionComponent implements OnInit {
 
-  constructor(private rs :ReservationService) { }
+  constructor(private rs :ReservationService) {
+   }
 
   ngOnInit() { }
 
@@ -19,9 +21,11 @@ export class DateSelectionComponent implements OnInit {
   startDate :Date;
   endDate :Date;
   message :string;
-
+  dailyCost :number = 50;
+  daysReserved :number;
   rooms :Observable<Room> ;
-  availableRoom :Room;
+  availableRooms :Room;
+  selectedRoomIds :Array<number> = [];
 
   submitDates() {
     this.rooms = this.rs.getRooms(this.startDate, this.endDate);
@@ -37,12 +41,33 @@ export class DateSelectionComponent implements OnInit {
     }
     else if ( this.startDate < this.endDate){
 
+      this.daysReserved = (new Date(this.endDate).getTime() - new Date(this.startDate).getTime()) / (1000 * 60 * 60 * 24);
+      console.log(this.daysReserved);
+
       this.rooms.subscribe(
       (response)=>{console.log("successful call" + response);
-          this.availableRoom = response
-          console.log(this.availableRoom)},
+          this.availableRooms = response
+          console.log(this.availableRooms)
+        },
       (response)=>{console.log("unsuccessful call" + response)}
       );
     }
+  }
+
+  addOrRemoveRoom(room_id :number) {
+    console.log(room_id);
+    if (this.selectedRoomIds.includes(room_id)) {
+      console.log("deleting");
+      // delete this.selectedRoomIds[room_id];
+      for(let i = 0; i < this.selectedRoomIds.length; i++ ){
+        if (this.selectedRoomIds[i] == room_id){
+          this.selectedRoomIds.splice(i, 1);
+        }
+      }
+    } else {
+      console.log("adding")
+      this.selectedRoomIds.push(room_id);
+    }
+    console.log(this.selectedRoomIds);
   }
 }
--- Project2/src/app/models/Reservation.ts
+++ Project2/src/app/models/Reservation.ts
@@ -1,8 +1,11 @@
+import {Room} from 'src/app/models/Room';
+
 export class Reservation{
     reservation_id :number;
     customer_id :number;
     start_date :string;
     end_date :string;
+    reserved_rooms :Array<Room>
     constructor(reservation_id :number, customer_id :number, start_date :string, end_date :string){
     this.reservation_id = reservation_id;
     this.customer_id = customer_id;
